4b103b1caedfc66ca6a78c138172c28ebfd7942a
Cody-Hayes97 <hayesmichael2@gmail.com>
2019-09-22 21:09:45 -0700

    Almost finished mvp just need to style

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { tsPropertySignature } from "@babel/types";
 
-const NumberButton = () => {
+const NumberButton = props => {
   return (
     <>
+      <button className="buttons numbers">{props.num}</button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
 };
+
+export default NumberButton;
